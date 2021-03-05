08570bd5ffb1c154d85098cd3a9b49d466eaf0d3
Rhys <mrevinz@gmail.com>
2021-03-05 16:13:28 +0000

    faff

--- GuildedRose/Store.cs
+++ GuildedRose/Store.cs
@@ -3,70 +3,23 @@ using System.Linq;
 
 namespace GuildedRose
 {
-    public class Store
+    public static class ItemTypes
     {
-        private class ItemTypes
-        {
-            public const string AgedBrie = "Aged Brie";
-            public const string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
-            public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
-        }
-        
-        public static void UpdateQuality(IList<Item> items)
+        public const string AgedBrie = "Aged Brie";
+        public const string BackstagePasses = "Backstage passes to a TAFKAL80ETC concert";
+        public const string Sulfuras = "Sulfuras, Hand of Ragnaros";
+    }
+    
+    public static class Store
+    {
+        public static void UpdateQuality(IEnumerable<Item> items)
         {
             foreach (var item in items.Where(i => i.Name != ItemTypes.Sulfuras && i.Quality < 50))
             {
-                if (item.Name != ItemTypes.AgedBrie && item.Name != ItemTypes.BackstagePasses)
-                {
-                    if (item.Quality > 0)
-                    {
-                        item.Quality = item.Quality - 1;
-                    }
-                }
-                else
-                {
-                    item.IncreaseQuality();
+                item.UpdateQuality();
 
-                    if (item.Name == ItemTypes.BackstagePasses)
-                    {
-                        if (item.SellIn < 11)
-                        {
-                            item.IncreaseQuality();
-                        }
-
-                        if (item.SellIn < 6)
-                        {
-                            item.IncreaseQuality();
-                        }
-                    }
-                }
-
-                item.SellIn = item.SellIn - 1;
-
-                if (item.SellIn < 0)
-                {
-                    if (item.Name != ItemTypes.AgedBrie)
-                    {
-                        if (item.Name != ItemTypes.BackstagePasses)
-                        {
-                            if (item.Quality > 0)
-                            {
-                                if (item.Name != ItemTypes.Sulfuras)
-                                {
-                                    item.Quality = item.Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            item.Quality = item.Quality - item.Quality;
-                        }
-                    }
-                    else
-                    {
-                        item.IncreaseQuality();
-                    }
-                }
+                item.SellIn--;
+                if (item.SellIn >= 0) continue;
             }
         }
     }
