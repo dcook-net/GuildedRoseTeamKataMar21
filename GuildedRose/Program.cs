91c45db2171af767eb812b3fdd8daaeb7dd5f0ee
Rhys <mrevinz@gmail.com>
2021-03-05 14:19:42 +0000

    tests & Small refactors

--- GuildedRose/Program.cs
+++ GuildedRose/Program.cs
@@ -1,117 +1,22 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace GuildedRose
 {
     public class Program
     {
-        IList<Item> Items;
-        
         public static void Main(string[] args)
         {
-            var app = new Program
-            {
-              Items = new List<Item>
-              {
-                  new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
-                  new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
-                  new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
-                  new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
-                  new Item
-                      {
-                          Name = "Backstage passes to a TAFKAL80ETC concert",
-                          SellIn = 15,
-                          Quality = 20
-                      },
-                  new Item {Name = "Conjured Mana Cake", SellIn = 3, Quality = 6}
-              }
-            };
-
-            foreach (var item in app.Items)
-                Console.WriteLine($"Item: {item.Name}. Quality: {item.Quality}. Sellin: {item.SellIn}");
-            
-            app.UpdateQuality();
-
-            foreach (var item in app.Items)
-                Console.WriteLine($"Item: {item.Name}. Quality: {item.Quality}. Sellin: {item.SellIn}");
-            
-            Console.ReadKey();
-        }
-
-        public void UpdateQuality()
-        {
-            for (var i = 0; i < Items.Count; i++)
-            {
-                if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                {
-                    if (Items[i].Quality > 0)
-                    {
-                        if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                        {
-                            Items[i].Quality = Items[i].Quality - 1;
-                        }
-                    }
-                }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-
-                        if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].SellIn < 11)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-
-                            if (Items[i].SellIn < 6)
-                            {
-                                if (Items[i].Quality < 50)
-                                {
-                                    Items[i].Quality = Items[i].Quality + 1;
-                                }
-                            }
-                        }
-                    }
-                }
-
-                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                {
-                    Items[i].SellIn = Items[i].SellIn - 1;
-                }
-
-                if (Items[i].SellIn < 0)
-                {
-                    if (Items[i].Name != "Aged Brie")
-                    {
-                        if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                        {
-                            if (Items[i].Quality > 0)
-                            {
-                                if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                                {
-                                    Items[i].Quality = Items[i].Quality - 1;
-                                }
-                            }
-                        }
-                        else
-                        {
-                            Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                        }
-                    }
-                    else
-                    {
-                        if (Items[i].Quality < 50)
-                        {
-                            Items[i].Quality = Items[i].Quality + 1;
-                        }
-                    }
-                }
-            }
+            // var store = new Store();
+            //
+            // foreach (var item in store.Items)
+            //     Console.WriteLine($"Item: {item.Name}. Quality: {item.Quality}. Sellin: {item.SellIn}");
+            //
+            // Store.UpdateQuality(store.Items);
+            //
+            // foreach (var item in store.Items)
+            //     Console.WriteLine($"Item: {item.Name}. Quality: {item.Quality}. Sellin: {item.SellIn}");
+            //
+            // Console.ReadKey();
         }
     }
 }
--- GuildedRoseTests/Tests.cs
+++ GuildedRoseTests/Tests.cs
@@ -1,10 +1,143 @@
+using System.Collections.Generic;
+using FluentAssertions;
+using GuildedRose;
 using NUnit.Framework;
 
 namespace GildedRoseTests
 {
     public class Tests
     {
+        private List<Item> _items = new List<Item>
+        {
+            new Item {Name = "+5 Dexterity Vest", SellIn = 10, Quality = 20},
+            new Item {Name = "Aged Brie", SellIn = 2, Quality = 0},
+            new Item {Name = "Elixir of the Mongoose", SellIn = 5, Quality = 7},
+            new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+            new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                SellIn = 15,
+                Quality = 20
+            },
+            new Item { Name = "Conjured Mana Cake", SellIn = 3, Quality = 6 }
+        };
+
         [Test]
-        public void ShouldPass() => Assert.Pass();
+        public void HappyPath()
+        {
+            Store.UpdateQuality(_items);
+            var updatedItems = new List<Item>
+            {
+                new Item {Name = "+5 Dexterity Vest", SellIn = 9, Quality = 19},
+                new Item {Name = "Aged Brie", SellIn = 1, Quality = 1},
+                new Item {Name = "Elixir of the Mongoose", SellIn = 4, Quality = 6},
+                new Item {Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 14,
+                    Quality = 21
+                },
+                new Item { Name = "Conjured Mana Cake", SellIn = 2, Quality = 5 }
+            };
+
+            updatedItems.Should().BeEquivalentTo(_items);
+        }
+        
+        [Test]
+        public void SulfurasShouldNeverDegradeInQuality()
+        {
+            var item =  new Item { Name = "Sulfuras, Hand of Ragnaros", SellIn = 0, Quality = 80 };
+            Store.UpdateQuality(new List<Item> { item });
+            Assert.That(item.Quality, Is.EqualTo(80));
+            Assert.That(item.SellIn, Is.EqualTo(0));
+        }
+        
+        [TestCase("+5 Dexterity Vest", 10, 20)]
+        [TestCase("Elixir of the Mongoose", 5, 7)]
+        [TestCase("Conjured Mana Cake", 5, 7)]
+        public void ItemsShouldDegradeInQualityAndQuantity(string name, int sellIn, int quality)
+        {
+            var item =  new Item { Name = "+5 Dexterity Vest", SellIn = sellIn, Quality = quality };
+            Store.UpdateQuality(new List<Item> { item });
+            Assert.That(item.Quality, Is.EqualTo(quality - 1));
+            Assert.That(item.SellIn, Is.EqualTo(sellIn - 1));
+        }
+        
+        [Test]
+        public void AgedBrieGoesUpInQualityAsAgeIncreases()
+        {
+            var item = new Item { Name = "Aged Brie", SellIn = 2, Quality = 0 };
+            Store.UpdateQuality(new List<Item> { item });
+            Assert.That(item.Quality, Is.EqualTo(1));
+            Assert.That(item.SellIn, Is.EqualTo(1));
+        }
+        
+        [Test]
+        public void ItemsCannotHaveAQualityOver50()
+        {
+            var items = new List<Item>
+            {
+                new Item {Name = "Aged Brie", SellIn = 2, Quality = 50},
+                new Item
+                {
+                    Name = "Backstage passes to a TAFKAL80ETC concert",
+                    SellIn = 4,
+                    Quality = 50
+                }
+            };
+            Store.UpdateQuality(items);
+            Assert.That(items[0].Quality, Is.EqualTo(50));
+            Assert.That(items[1].Quality, Is.EqualTo(50));
+        }
+        
+        [TestCase(10, 20, 2)]
+        [TestCase(9, 20, 2)]
+        [TestCase(5, 20, 3)]
+        [TestCase(4, 20, 3)]
+        [TestCase(1, 20, 3)]
+        [TestCase(0, 20, -20)]
+        public void BackstagePassMeetsQualityRules(int sellIn, int quality, int increase)
+        {
+            var item = new Item
+            {
+                Name = "Backstage passes to a TAFKAL80ETC concert",
+                SellIn = sellIn,
+                Quality = quality
+            };
+            Store.UpdateQuality(new List<Item> { item });
+            Assert.That(item.Quality, Is.EqualTo(quality + increase));
+            Assert.That(item.SellIn, Is.EqualTo(sellIn - 1));
+        }
+        
+        [TestCase("+5 Dexterity Vest", 20)]
+        [TestCase("Elixir of the Mongoose", 5)]
+        [TestCase("Conjured Mana Cake", 5 )]
+        public void OnceSellInDateHasPassedQualityDegradesTwiceAsFast(string name, int quality)
+        {
+            var item = new Item
+            {
+                Name = name,
+                SellIn = 0,
+                Quality = quality
+            };
+            Store.UpdateQuality(new List<Item> { item });
+            Assert.That(item.Quality, Is.EqualTo(quality - 2));
+        }
+        
+        [TestCase("+5 Dexterity Vest")]
+        [TestCase("Elixir of the Mongoose")]
+        [TestCase("Conjured Mana Cake")]
+        public void ItemQualityCanNeverGoBelowZero(string name)
+        {
+            var item = new Item
+            {
+                Name = name,
+                SellIn = 1,
+                Quality = 0
+            };
+            Store.UpdateQuality(new List<Item> { item });
+            Assert.That(item.Quality, Is.EqualTo(0));
+        }
     }
 }
